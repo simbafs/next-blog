b095ff59a4d17517210a38b36b060d66595b5c76
SimbaFs <simba.fs@gmail.com>
2023-06-29 11:44:01 +0800

    change theme to solarized

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,39 @@ module.exports = {
     fontFamily: {
       mono: ['Hack'],
     },
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-headings': theme('colors.base3'),
+            '--tw-prose-body': theme('colors.base2'),
+            '--tw-prose-counters': theme('colors.base2'),
+            '--tw-prose-code': theme('colors.base2'),
+            '--tw-prose-bold': theme('colors.base2'),
+            '--tw-prose-links': theme('colors.base2'),
+            '--tw-prose-quotes': theme('colors.base1'),
+          },
+        },
+      }),
+    },
+    colors: {
+      base03: '#002b36',
+      base02: '#073642',
+      base01: '#586e75',
+      base00: '#657b83',
+      base0: '#839496',
+      base1: '#93a1a1',
+      base2: '#eee8d5',
+      base3: '#fdf6e3',
+      yellow: '#b58900',
+      orange: '#cb4b16',
+      red: '#dc322f',
+      magenta: '#d33682',
+      violet: '#6c71c4',
+      blue: '#268bd2',
+      cyan: '#2aa198',
+      green: '#859900',
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
